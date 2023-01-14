820c9945329275f5eae31771c1d3098185967141
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2023-01-14 15:41:00 -0600

    Add settings to redux store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import settings from './settings/settings';
 import chips from './chips/chips';
 
 const rootReducer = combineReducers({
+  settings,
   chips,
 });
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
---
+++ src/redux/settings/settings.js
@@ -0,0 +1,35 @@
+const initialState = {
+  showGrid: true,
+  doSnap: true,
+  globalSize: 16,
+};
+
+const TOGGLE_GRID = 'settings/settings/TOGGLE_GRID';
+const TOGGLE_SNAP = 'settings/settings/TOGGLE_SNAP';
+
+function toggleGrid(data) {
+  return {
+    type: TOGGLE_GRID,
+    payload: data,
+  };
+}
+
+function toggleSnap(data) {
+  return {
+    type: TOGGLE_SNAP,
+    payload: data,
+  };
+}
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case TOGGLE_GRID:
+      return { ...state, showGrid: action.payload };
+    case TOGGLE_SNAP:
+      return { ...state, doSnap: action.payload };
+    default:
+      return state;
+  }
+}
+
+export { toggleGrid, toggleSnap };
