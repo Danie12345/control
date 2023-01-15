bf4fb1ce182278ecbf9b385314e67d611aa84e7a
Daniel Malo Osorio <danielmaloosorio@gmail.com>
2023-01-14 18:31:43 -0600

    Add zoom functionality to store

--- src/redux/settings/settings.js
+++ src/redux/settings/settings.js
@@ -2,11 +2,12 @@ const initialState = {
   showGrid: true,
   doSnap: true,
   globalSize: 16,
-  zoom: 1,
+  zoom: 1.0,
 };
 
 const TOGGLE_GRID = 'settings/settings/TOGGLE_GRID';
 const TOGGLE_SNAP = 'settings/settings/TOGGLE_SNAP';
+const APPLY_ZOOM = 'settings/settings/APPLY_ZOOM';
 
 function toggleGrid(data) {
   return {
@@ -22,15 +23,24 @@ function toggleSnap(data) {
   };
 }
 
+function applyZoom(data) {
+  return {
+    type: APPLY_ZOOM,
+    payload: data,
+  };
+}
+
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case TOGGLE_GRID:
       return { ...state, showGrid: action.payload };
     case TOGGLE_SNAP:
       return { ...state, doSnap: action.payload };
+    case APPLY_ZOOM:
+      return { ...state, zoom: action.payload };
     default:
       return state;
   }
 }
 
-export { toggleGrid, toggleSnap };
+export { toggleGrid, toggleSnap, applyZoom };
